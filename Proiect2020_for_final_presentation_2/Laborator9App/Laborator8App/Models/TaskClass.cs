19f00019647dd36f2a4c9a696e2977763fc0d8e8
Constantin Gabriel <39054700+Kira060200@users.noreply.github.com>
2021-01-06 12:13:47 +0200

    Final

---
+++ Proiect2020_for_final_presentation_2/Laborator9App/Laborator8App/Models/TaskClass.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Laborator8App.Models;
+namespace Laborator8App.Models
+{
+    public class TaskClass
+    {
+        [Key]
+        public int TaskId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Status { get; set; }
+        public string Content { get; set; }
+        public string WorkerName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int ProjectId { get; set; }
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public virtual Project Project { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
