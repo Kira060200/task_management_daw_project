7bf27f4628e8fb4a51d9f859f951087a75e264e2
Constantin Gabriel <39054700+Kira060200@users.noreply.github.com>
2021-01-06 12:15:13 +0200

    Final

---
+++ Proiect2020_for_final_presentation_3/Laborator9App/Laborator8App/Models/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Laborator8App.Models;
+
+namespace Laborator8App.Models
+{
+    public class Project
+    {
+        [Key]
+        public int ProjectId { get; set; }
+        [Required]
+        public string ProjectName { get; set; }
+        public int TeamId { get; set; }
+
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public virtual Team Team { get; set; }
+        public virtual ICollection<TaskClass> Tasks { get; set; }
+    }
+}
